3c81c1151b17208b53d6b9e5faa1cb7613c1f873
huoxudong125 <423huo@163.com>
2017-02-08 14:07:00 +0800

    Add OpacityMaskView

---
+++ HQF.WPF.Mask/ViewModels/OpacityMaskViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HQF.WPF.Mask.ViewModels
+{
+    public class OpacityMaskViewModel : BindableBase
+    {
+        public OpacityMaskViewModel()
+        {
+
+        }
+    }
+}
---
+++ HQF.WPF.Mask/Views/OpacityMaskView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HQF.WPF.Mask.Views
+{
+    /// <summary>
+    /// Interaction logic for OpacityMaskView
+    /// </summary>
+    public partial class OpacityMaskView : UserControl
+    {
+        public OpacityMaskView()
+        {
+            InitializeComponent();
+        }
+    }
+}
