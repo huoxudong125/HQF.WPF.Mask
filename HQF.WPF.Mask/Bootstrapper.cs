48c6690b7e9013e705b1ea59a2b166fc7de70864
huoxudong125 <423huo@163.com>
2017-02-08 14:04:27 +0800

    Add project files.

---
+++ HQF.WPF.Mask/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using HQF.WPF.Mask.Views;
+using System.Windows;
+
+namespace HQF.WPF.Mask
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
